a47c8b7b50b9e1b96732c420edb39cea596de9a2
Moritz Schute <abo.ms@outlook.com>
2023-12-31 15:07:59 +0100

    Plot mit skalierten Größen und Farben und Funktion für Kräfteberechnung (noch ineffizient)

--- Himmelskoerper_Class.py
+++ Himmelskoerper_Class.py
@@ -5,19 +5,27 @@ Created on Mon Dec 18 20:34:35 2023
 @author: Moritz Schute
 """
 import matplotlib.pyplot as plt
+import math
 
 
 class Himmelskoerper:
 	"""Klasse für abstrakte Himmelskörper."""
 	
 	#Klassenvariablen: Plot Sachen
-	__fig = plt.figure(figsize= (10,8))
+	__fig = plt.figure(figsize= (10,6))
 	__ax = plt.axes(projection='3d')
-	__ax.set_xlabel('x in AE')
-	__ax.set_ylabel('y in AE')
-	__ax.set_zlabel('z in AE')
+	__ax.set_xlabel('x in m')
+	__ax.set_ylabel('y in m')
+	__ax.set_zlabel('z in m')
+	__ax.set
+
+	#Klassendictonary: Farbcode
+	__color_dict = {'STA': 'y', #Stern: gelb
+				'PLA': 'b', #Planet: blau
+				'DWA': 'g', #Zwergplanet: grün
+				'SAT': 'r'} #Satellit: rot
 	
-	def __init__(self, specifications):
+	def __init__(self, countable_specifications, type):
 		"""
 		Weise Instanz Attribute zu (Masse, Position, Startgeschwindigkeit)
 
@@ -31,16 +39,26 @@ class Himmelskoerper:
 		None.
 
 		"""
-		self.__id = specifications[0]
-		self.__mass = specifications[1]
-		self.pos_x = specifications[2]
-		self.pos_y = specifications[3]
-		self.pos_z = specifications[4]
-		self.vel_x = specifications[5]
-		self.vel_y = specifications[6]
-		self.vel_z = specifications[7]
+		#Umrechnungsfaktor, da 
+		AE = 1.496*10**11
+
+		self.id = countable_specifications[0]
+		self.mass = countable_specifications[1]
+		self.pos_x = countable_specifications[2] * AE
+		self.pos_y = countable_specifications[3] * AE
+		self.pos_z = countable_specifications[4] * AE
+		self.vel_x = countable_specifications[5] * AE / 86400
+		self.vel_y = countable_specifications[6] * AE / 86400
+		self.vel_z = countable_specifications[7] * AE / 86400
+		self.type = type
+		self.color = self.__color_dict[type]
+		self.point_size = math.pow(math.log(self.mass, 10**16), 10)
 	
 	def plot_Himmelskoerper (self):
 		"""Plotte Instanz von Himmelskörper."""
-		self.__ax.scatter(self.pos_x, self.pos_y, self.pos_z)
+
+		self.__ax.scatter(self.pos_x, self.pos_y, self.pos_z, s = self.point_size, c = self.color)	
+
 		
+	
+
--- main.py
+++ main.py
@@ -11,15 +11,55 @@ import numpy as np
 
 # Lade Daten aus CSV-Datei, Überspringe erste Zeile, lasse erstmal Spalten
 # 1 und 2 weg, da Name und Typ (Star, Planet,...) erstmal irrelevant.
-rohdaten_array = np.genfromtxt("planets_and_moons.csv",
+zahl_rohdaten_array = np.genfromtxt("planets_and_moons.csv",
 								  delimiter= ",", skip_header=1, 
 								  usecols=(0,3,4,5,6,7,8,9))
+typ_rohdaten_array = np.genfromtxt("planets_and_moons.csv", dtype=np.str_,
+								  delimiter= ",", skip_header=1, 
+								  usecols=2)
 
 #Speichere alle Himmelskörper mit List Comprehension in Array
-himmelskoerper_array = [Himmelskoerper(rohdaten_array[index]) 
-						for index in range (len(rohdaten_array))]
+himmelskoerper_array = [Himmelskoerper(zahl_rohdaten_array[index], typ_rohdaten_array[index]) 
+						for index in range (len(zahl_rohdaten_array))]
 
 #Plotte alle Himmelskörper erstmal zum Test 
 for object in himmelskoerper_array:
 	object.plot_Himmelskoerper()
 plt.show()
+
+"""
+
+def calculate_res_forces (himmelskoerper_array):
+	#Anzahl Körper
+	GRAVITATION_CONSTANT = 6.67*10**-11
+	l = len(himmelskoerper_array)
+	force_array = np.zeros((l, 3))
+	for index1, erster_koerper in enumerate(himmelskoerper_array):
+		sum_x = 0
+		sum_y = 0
+		sum_z = 0
+		for index2, zweiter_koerper in enumerate(himmelskoerper_array):
+			if index1 != index2:
+				distanzvektor = np.array([zweiter_koerper.pos_x - erster_koerper.pos_x,
+							   zweiter_koerper.pos_y - erster_koerper.pos_y,
+							   zweiter_koerper.pos_z - erster_koerper.pos_z])
+				abstand = np.sqrt(np.dot(distanzvektor, distanzvektor))
+				normierter_distanzvektor = distanzvektor / abstand
+				sum_x += (GRAVITATION_CONSTANT * erster_koerper.mass * zweiter_koerper.mass 
+										/ abstand**2 * normierter_distanzvektor[0])
+				sum_y += (GRAVITATION_CONSTANT * erster_koerper.mass * zweiter_koerper.mass 
+										/ abstand**2 * normierter_distanzvektor[1])
+				sum_z += (GRAVITATION_CONSTANT * erster_koerper.mass * zweiter_koerper.mass 
+										/ abstand**2 * normierter_distanzvektor[2])	
+				if(index1 == 0 and index2 == 1):
+					print(GRAVITATION_CONSTANT * erster_koerper.mass * zweiter_koerper.mass 
+										/ abstand**2)	
+		force_array[index1, 0] = sum_x
+		force_array[index1, 1] = sum_y
+		force_array[index1, 2] = sum_z
+						
+	return force_array
+
+print(calculate_res_forces(himmelskoerper_array))			
+
+"""
